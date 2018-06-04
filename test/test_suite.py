0896f8625679444cb8c8e72132146782add99b0c
MGlauer <martinglauer89@gmail.com>
2018-06-04 15:39:39 +0200

    Organize test suite more controllably

--- test/test_suite.py
+++ test/test_suite.py
@@ -1,3 +1,11 @@
 # test/test_suite.py
 
-from sqlalchemy.testing.suite  import *
+from sqlalchemy.testing.suite.test_dialect import *
+from sqlalchemy.testing.suite.test_ddl import *
+from sqlalchemy.testing.suite.test_insert import *
+from sqlalchemy.testing.suite.test_sequence import *
+from sqlalchemy.testing.suite.test_select import *
+#from sqlalchemy.testing.suite.test_results import *
+from sqlalchemy.testing.suite.test_update_delete import *
+# from sqlalchemy.testing.suite.test_reflection import *
+# from sqlalchemy.testing.suite.test_types import *
