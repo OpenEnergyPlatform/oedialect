273660b9ad8528e1867e872fc39e53acc0b0054a
MGlauer <martinglauer89@gmail.com>
2019-09-03 14:13:21 +0200

    Unfold imports in test_suite.py

--- test/test_suite.py
+++ test/test_suite.py
@@ -1,11 +1,59 @@
 # test/test_suite.py
 
-from sqlalchemy.testing.suite.test_dialect import *
-from sqlalchemy.testing.suite.test_ddl import *
-from sqlalchemy.testing.suite.test_insert import *
-from sqlalchemy.testing.suite.test_sequence import *
-from sqlalchemy.testing.suite.test_select import *
-#from sqlalchemy.testing.suite.test_results import *
-from sqlalchemy.testing.suite.test_update_delete import *
-# from sqlalchemy.testing.suite.test_reflection import *
-# from sqlalchemy.testing.suite.test_types import *
+from sqlalchemy.testing.suite.test_dialect import (
+    AutocommitTest,
+    EscapingTest,
+    ExceptionTest,
+)
+from sqlalchemy.testing.suite.test_ddl import TableDDLTest
+
+from sqlalchemy.testing.suite.test_insert import (
+    LastrowidTest,
+    InsertBehaviorTest,
+    ReturningTest,
+)
+from sqlalchemy.testing.suite.test_sequence import (
+    HasSequenceTest,
+    SequenceCompilerTest,
+    SequenceTest,
+)
+from sqlalchemy.testing.suite.test_select import (
+    LimitOffsetTest,
+    ExpandingBoundInTest,
+    OrderByLabelTest,
+    LikeFunctionsTest,
+    CollateTest,
+    CompoundSelectTest,
+)
+
+from sqlalchemy.testing.suite.test_results import (
+    PercentSchemaNamesTest,
+    RowFetchTest,
+    ServerSideCursorsTest,
+)
+from sqlalchemy.testing.suite.test_update_delete import SimpleUpdateDeleteTest
+
+from sqlalchemy.testing.suite.test_reflection import (
+    ComponentReflectionTest,
+    HasTableTest,
+    NormalizedNameTest,
+)
+from sqlalchemy.testing.suite.test_types import (
+    UnicodeVarcharTest,
+    UnicodeTextTest,
+    JSONTest,
+    DateTest,
+    DateTimeTest,
+    TextTest,
+    NumericTest,
+    IntegerTest,
+    DateTimeHistoricTest,
+    DateTimeCoercedToDateTimeTest,
+    TimeMicrosecondsTest,
+    TimestampMicrosecondsTest,
+    TimeTest,
+    DateTimeMicrosecondsTest,
+    DateHistoricTest,
+    StringTest,
+    BooleanTest,
+)
