db255a6e9a251adc3432a276c54fe54d93b5430e
c.winger <c.winger@oeko.de>
2025-07-22 19:03:43 +0200

    fix test script

--- test/test.py
+++ test/test.py
@@ -1,71 +1,79 @@
-from sqlalchemy import *
+"""Test script that can be run directly, e.g. `python -m test.test`"""
+
+# from test.login import DB_CREDS
+from test.login import OED_CREDS
+
+from sqlalchemy import INTEGER, VARCHAR, Column, MetaData, Table, create_engine
 from sqlalchemy.orm import sessionmaker
-from .login import OED_CREDS, DB_CREDS
 
-import oedialect
+import oedialect  # noqa
 
-OED_STRING = 'postgresql+oedialect://{creds}@localhost:8000'.format(creds=OED_CREDS)
-DB_STRING = 'postgresql://{creds}@localhost:5432'.format(creds=DB_CREDS)
+# DB_STRING = "postgresql://{creds}@localhost:5435/oedb".format(creds=DB_CREDS)
+# NOTE: also set environment variable OEDIALECT_PROTOCOL=http
+DB_STRING = "postgresql+oedialect://{creds}@localhost:8000".format(creds=OED_CREDS)
 
-if __name__ == '__main__':
 
-    engine = create_engine(OED_STRING)
+if __name__ == "__main__":
+
+    engine = create_engine(DB_STRING)
     metadata = MetaData(bind=engine)
 
-    tname = 'oedtest'
-    sname = 'sandbox'
+    tname = "oedtest"
+    sname = "sandbox"
 
-    table = Table(tname, metadata,
-                  Column('name', VARCHAR(50)),
-                  Column('age', INTEGER),
-                  schema=sname)
+    table = Table(
+        tname,
+        metadata,
+        Column("name", VARCHAR(50)),
+        Column("age", INTEGER),
+        schema=sname,
+    )
 
-    print('Created table')
+    print("Created table")
 
     conn = engine.connect()
     try:
         Session = sessionmaker(bind=engine)
         if not engine.dialect.has_table(conn, tname, sname):
-            table.create()
-
+            table.create()  # type: ignore
 
             session = Session()
             try:
-                insert_statement = table.insert().values(
+                insert_statement = table.insert().values(  # type: ignore
                     [
-                        dict(name='Peter', age=25),
-                        dict(name='Inge', age=42),
-                        dict(name='Horst', age=36)
+                        dict(name="Peter", age=25),
+                        dict(name="Inge", age=42),
+                        dict(name="Horst", age=36),
                     ]
                 )
                 session.execute(insert_statement)
                 session.commit()
-            except Exception as e:
+            except Exception:
                 session.rollback()
                 raise
             finally:
                 session.close()
 
-        print('Inserted data')
+        print("Inserted data")
 
         session = Session()
         try:
 
-            result = session.query(table).filter(table.c.age>30)
+            result = session.query(table).filter(table.c.age > 30)  # type: ignore
 
             if result:
                 for row in result:
                     print(row)
-        except Exception as e:
+        except Exception:
             session.rollback()
             raise
         finally:
             session.close()
 
-        print('Queried dataset')
+        print("Queried dataset")
 
-        table.drop()
+        table.drop()  # type: ignore
 
-        print('Drop table')
+        print("Drop table")
     finally:
         conn.close()
