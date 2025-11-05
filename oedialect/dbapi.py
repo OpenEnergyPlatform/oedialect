b38a6941db57381fdfb8b9809f640b26b7ccbd25
c.winger <c.winger@oeko.de>
2025-11-05 07:47:02 +0100

    added required Error object

--- oedialect/dbapi.py
+++ oedialect/dbapi.py
@@ -1,4 +1,8 @@
+from psycopg2._psycopg import Error
+
 from oedialect.engine import OEConnection
+
+__all__ = ["Error", "connect"]  # required for dialect
 
 
 def connect(dsn=None, connection_factory=None, cursor_factory=None, **kwargs):
